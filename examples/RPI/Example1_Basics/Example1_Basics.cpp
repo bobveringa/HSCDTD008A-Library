de0cac344c33ed8c3bd4535c948e28202b4afa0c
Tilman Gloetzner <tilman_1@gloetzner.net>
2022-09-07 01:33:54 +0200

    - Extended library for RPI - Migrated Example1_Basic to RPI 	new file:   examples/RPI/Example1_Basics/Example1_Basics 	new file:   examples/RPI/Example1_Basics/Example1_Basics.cpp 	new file:   examples/RPI/Example1_Basics/Makefile 	new file:   src/driver/platform_rpi.cpp

---
+++ examples/RPI/Example1_Basics/Example1_Basics.cpp
@@ -0,0 +1,67 @@
+/****************************************************************
+ * Example1_Basics.cpp
+ * HSCDTD008A Library Demo
+ * Tilman Glötzner
+ * Original Creation Date: 2022-09-03
+ * Heavily based on the code of Bob Veringa
+ *
+ * Distributed as-is; no warranty is given.
+ ***************************************************************/
+
+#include "hscdtd008a.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+// Create an instance of the sensor.
+HSCDTD008A geomag;
+
+
+void setup() {
+  hscdtd_status_t status;
+
+
+  geomag.begin();
+  // If you know the I2C address is different than in the provided
+  // data sheet. Uncomment the line below, and configure the address.
+  geomag.begin(0x0F);
+
+  // Initialize the hardware.
+  status = geomag.initialize();
+  if (status != HSCDTD_STAT_OK) {
+    printf("Failed to initialize sensor. Status:%d. Check wiring.\n",status);
+
+    // Halt program here.
+    exit(1);
+  }
+  // Compensate for temperature.
+  geomag.temperatureCompensation();
+
+}
+
+void loop() {
+  hscdtd_status_t status;
+
+  // Explicitly start a reading.
+  status = geomag.startMeasurement();
+  // If the status is OK then we can print the result.
+  if (status == HSCDTD_STAT_OK) {
+    float Babs = sqrt(pow(geomag.mag.mag_x,2) + pow(geomag.mag.mag_y,2) + pow(geomag.mag.mag_z,2));
+    printf("X: %f uT,\tY: %f uT,\tZ: %f uT;\t|B|: %f uT\n",
+    	geomag.mag.mag_x, geomag.mag.mag_y, geomag.mag.mag_z,Babs);
+  } else {
+    // If it is not OK, then we don't print anything.
+    printf("Error occurred, unable to read sensor data.\n");
+    exit(1);
+  }
+  // Wait 50ms  before reading the next sample.
+  usleep(50000);
+}
+
+int main(int argc, char** argv)
+{
+   setup(); 
+   loop();
+}
+
---
+++ src/driver/platform_rpi.cpp
@@ -0,0 +1,140 @@
+
+/****************************************************************
+ * platform_rpi.cpp   
+ * HSCDTD008A Library Extention for RPI
+ * Tilman Glötzner
+ * Original Creation Date: 2022-09-07
+ *
+ * Distributed as-is; no warranty is given.
+ ***************************************************************/
+
+
+#include "platform.h"
+#include "hscdtd008a_driver.h"
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <linux/i2c-dev.h>
+#define I2C_ADDR 0x0f
+#define device "/dev/i2c-1"
+#ifndef I2C_M_RD
+#include <linux/i2c.h>
+#endif
+
+int fd = -1;
+
+int8_t t_open(void)
+{
+    fd = open(device, O_RDWR); 
+    if (fd >= 0)
+    {
+       if (ioctl(fd, I2C_SLAVE, I2C_ADDR) < 0) {
+        printf("ioctl error: %s\n", strerror(errno));
+        return 1;
+       }
+    }   
+    return (fd<0);
+}
+
+int8_t t_read_register(uint8_t addr,
+                       uint8_t reg,
+                       uint8_t length,
+                       uint8_t *p_buffer)
+{
+    int8_t status;
+    uint8_t inbuf[32];
+    uint8_t outbuf[1];
+    struct i2c_msg msgs[2];
+    struct i2c_rdwr_ioctl_data msgset[1];
+
+    msgs[0].addr = addr;
+    msgs[0].flags = 0;
+    msgs[0].len = 1;
+    msgs[0].buf = outbuf;
+ 
+    outbuf[0] = reg;
+    
+    msgs[1].addr = addr;
+    msgs[1].flags = I2C_M_RD | I2C_M_NOSTART;
+    msgs[1].len = length;
+    msgs[1].buf = inbuf;
+
+    msgset[0].msgs = msgs;
+    msgset[0].nmsgs = 2;
+
+
+    if (length >= 32) 
+      return HSCDTD_STAT_ERROR; 
+//    printf("t_read_register: reg=0x%x\n", reg);
+
+    *p_buffer = 0;
+    if (ioctl(fd, I2C_RDWR, &msgset) < 0) {
+        printf("ioctl(I2C_RDWR) in i2c_read");
+        printf("t_read_register: Error writing to i2c device: %s\n", 
+  		strerror(errno));
+        printf("t_read_register: length=%d, status =%d", length,status);
+
+        return HSCDTD_STAT_ERROR;
+    }
+
+    strncpy((char*)p_buffer,(char*)inbuf,length); 
+    return HSCDTD_STAT_OK;
+}
+
+int8_t t_write_register(uint8_t addr,
+                        uint8_t reg,
+                        uint8_t length,
+                        uint8_t *p_buffer)
+{
+    int8_t status;
+    uint8_t buffer[32];
+    struct i2c_msg msgs[1];
+    struct i2c_rdwr_ioctl_data msgset[1];
+
+    buffer[0] = reg;
+    if (length >= 31) 
+      return HSCDTD_STAT_ERROR; 
+
+    strncpy((char*)buffer+1, (char*) p_buffer, length);
+    
+    msgs[0].addr = addr;
+    msgs[0].flags = 0;
+    msgs[0].len = length+1;
+    msgs[0].buf = buffer;
+
+    msgset[0].msgs = msgs;
+    msgset[0].nmsgs = 1;
+
+    if (ioctl(fd, I2C_RDWR, &msgset) < 0)
+    {
+        printf("t_write_register: ioctl(I2C_RDWR) in i2c_write");
+	printf("Error writing to i2c device: %s.\nstatus=%d,length=%d\n", 
+		strerror(errno),status, length);
+        return HSCDTD_STAT_ERROR;
+    }
+
+    return HSCDTD_STAT_OK;
+}
+
+int8_t t_flush(void)
+{
+    return HSCDTD_STAT_OK;
+}
+
+
+int8_t t_close(void)
+{
+    close(fd);
+    return HSCDTD_STAT_OK;
+}
+
+
+void t_sleep_ms(uint32_t duration_ms)
+{
+    usleep(duration_ms*1000);
+}
